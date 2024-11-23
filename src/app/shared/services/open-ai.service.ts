9830594a313f888b6ed101c167195699e2b5efd1
egoulan <eddygoulan@gmail.com>
2024-11-23 19:48:22 +0100

    small fix

--- src/app/shared/services/open-ai.service.ts
+++ src/app/shared/services/open-ai.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { GridFromImageResponse } from 'src/app/data/models/grid-from-image-response';
 import { environment } from 'src/environments/environment';
 
@@ -14,48 +14,6 @@ export class OpenAiService {
   public analyzeImage(
     base64Image: string
   ): Observable<GridFromImageResponse[]> {
-    return of([
-      {
-        numero: 1,
-        quines: [
-          [11, 36, 59, 69, 87],
-          [19, 23, 41, 55, 74],
-          [7, 22, 44, 62, 81],
-        ],
-      },
-      {
-        numero: 2,
-        quines: [
-          [11, 36, 59, 69, 87],
-          [19, 23, 41, 55, 74],
-          [7, 22, 44, 62, 81],
-        ],
-      },
-      {
-        numero: 3,
-        quines: [
-          [11, 36, 59, 69, 87],
-          [19, 23, 41, 55, 74],
-          [7, 22, 44, 62, 81],
-        ],
-      },
-      {
-        numero: 4,
-        quines: [
-          [11, 36, 59, 69, 87],
-          [19, 23, 41, 55, 74],
-          [7, 22, 44, 62, 81],
-        ],
-      },
-      {
-        numero: 1,
-        quines: [
-          [11, 36, 59, 69, 87],
-          [19, 23, 41, 55, 74],
-          [7, 22, 44, 62, 81],
-        ],
-      },
-    ]);
     return this.http.post<GridFromImageResponse[]>(`${this.baseUrl}/analyze`, {
       base64Image,
       fileType: 'image',
